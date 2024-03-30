098ce3b47b3d3407047f1a2c4fc089759891ea5e
msisaif <msi313.bd@gmail.com>
2024-03-30 13:01:13 +0600

    initial setup for starting api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return [
+        'Laravel'   => app()->version(),
+        'PHP'       => PHP_VERSION,
+    ];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
